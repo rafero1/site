7fb37eca50f6e3fbcc5a69737702d770b5f47396
Rafael Sá <rafael.sa@lsbd.ufc.br>
2024-01-30 08:53:05 -0300

    Refactor primaryBar.vue component

--- components/primaryBar.vue
+++ components/primaryBar.vue
@@ -1,26 +1,21 @@
 <template>
-  <div
-    class="w-full p5 box-border flex flex-justify-between flex-items-start font-size-3 sm:font-size-4"
-  >
-    <span
-      ><span class="text-green">$</span> {{ buildFancyPath($props.path) }}</span
-    >
-    <div class="flex-self-end">
-      <LocaleSelector class="mr-5"></LocaleSelector>
-      <span>
-        {{ time.split(":")[0] }}<span class="clock-separator">:</span
-        >{{ time.split(":")[1] }}
-      </span>
+    <div class="w-full p5 box-border flex flex-justify-between flex-items-start font-size-3 sm:font-size-4">
+        <span><span class="text-green">$</span> {{ buildFancyPath($props.path) }}</span>
+        <div class="flex-self-end">
+            <LocaleSelector></LocaleSelector>
+            <span>
+                {{ time.split(":")[0] }}<span class="clock-separator">:</span>{{ time.split(":")[1] }}
+            </span>
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup lang="ts">
 const props = defineProps({
-  path: {
-    type: String,
-    required: true,
-  },
+    path: {
+        type: String,
+        required: true,
+    },
 });
 
 /**
@@ -28,26 +23,26 @@ const props = defineProps({
  * @returns the current time
  */
 const getCurrentTime = (): string => {
-  return new Date().toLocaleTimeString([], {
-    hour12: false,
-    hour: "2-digit",
-    minute: "2-digit",
-  });
+    return new Date().toLocaleTimeString([], {
+        hour12: false,
+        hour: "2-digit",
+        minute: "2-digit",
+    });
 };
 
 const time = ref(getCurrentTime());
 
 onMounted(() => {
-  // update the time every second
-  setInterval(() => {
-    time.value = getCurrentTime();
-  }, 1000);
+    // update the time every second
+    setInterval(() => {
+        time.value = getCurrentTime();
+    }, 1000);
 
-  // blink the clock separator every now and then
-  setInterval(() => {
-    const clock = document.querySelector(".clock-separator");
-    clock?.classList.toggle("invisible");
-  }, 2000);
+    // blink the clock separator every now and then
+    setInterval(() => {
+        const clock = document.querySelector(".clock-separator");
+        clock?.classList.toggle("invisible");
+    }, 2000);
 });
 
 /**
@@ -56,14 +51,14 @@ onMounted(() => {
  * @returns the fancy path string
  */
 const buildFancyPath = (path: string): string => {
-  let pathString = "/home/rafael";
+    let pathString = "/home/rafael";
 
-  if (path === "/") {
-    pathString += "/hello_world.sh";
-  } else {
-    pathString += path.toLowerCase().replace("-", "_") + ".sh";
-  }
+    if (path === "/") {
+        pathString += "/hello_world.sh";
+    } else {
+        pathString += path.toLowerCase().replace("-", "_") + ".sh";
+    }
 
-  return pathString;
+    return pathString;
 };
 </script>
